8f768a5a5b1a82cecea101052b0c382471651326
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-03-24 15:15:26 -0300

    .

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caixa Eletrônico</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <form action="" method="post">
+            <h1>Caixa Eletrônico</h1>
+            <label for="valor">Qual o valor deseja sacar?</label>
+            <input type="number" name="valor" id="valor">
+            <input type="submit" value="Sacar">
+        </form>
+    </main>
+    <section>
+        
+    </section>
+    
+</body>
+</html>
