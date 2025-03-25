d7f9144da53557ecd1a9ccb13f534fb786e45834
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-03-25 14:56:11 -0300

    .

--- public/login.php
+++ public/login.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
     $login=$_POST['login'];
     $password=$_POST['senha'];
     
-    header (Location:../../public/caixa.php);
+    header (Location:../public/caixa.php);
 }
 
 ?>
